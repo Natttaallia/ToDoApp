cafdef3342bad819d48ef395b383b2785f6f7d4c
nkulbaka <nkulbaka@eclassifiedsgroup.com>
2022-11-03 02:35:23 +0100

    Create DI modules

---
+++ app/src/main/java/com/example/todo/app/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.example.todo.app.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.todo.data.db.ToDoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDataBase(@ApplicationContext context: Context): ToDoDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            ToDoDatabase::class.java,
+            "Tasks.db"
+        ).build()
+    }
+}

b5123860b316cfaca0336ad98e1febd55089a223
nkulbaka <nkulbaka@eclassifiedsgroup.com>
2022-11-03 01:51:17 +0100

    Define TasksRepository

--- app/src/main/java/com/example/todo/domain/repositories/TasksRepository.kt
+++ app/src/main/java/com/example/todo/domain/repositories/TasksRepository.kt
@@ -1,4 +1,18 @@
 package com.example.todo.domain.repositories
 
+import com.example.todo.domain.models.Task
+
 interface TasksRepository {
+
+    suspend fun getTasks(): Result<List<Task>>
+
+    suspend fun getTask(taskId: String): Result<Task>
+
+    suspend fun saveTask(task: Task)
+
+    suspend fun completeTask(taskId: String)
+
+    suspend fun activateTask(taskId: String)
+
+    suspend fun deleteTask(taskId: String)
 }

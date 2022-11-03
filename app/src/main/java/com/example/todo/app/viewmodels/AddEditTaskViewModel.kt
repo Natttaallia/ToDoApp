b9126a1fc5ad53d4c1ac1688746211041906ff53
nkulbaka <nkulbaka@eclassifiedsgroup.com>
2022-11-03 03:44:27 +0100

    Add AddEditTask component

--- app/src/main/java/com/example/todo/app/viewmodels/AddEditTaskViewModel.kt
+++ app/src/main/java/com/example/todo/app/viewmodels/AddEditTaskViewModel.kt
@@ -1,9 +1,119 @@
 package com.example.todo.app.viewmodels
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todo.R
+import com.example.todo.app.data.states.AddEditTaskUiState
+import com.example.todo.app.navigation.TodoDestinationsArgs
+import com.example.todo.domain.models.Result
+import com.example.todo.domain.models.Task
+import com.example.todo.domain.repositories.TasksRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddEditTaskViewModel @Inject constructor() : ViewModel() {
+class AddEditTaskViewModel @Inject constructor(
+    private val tasksRepository: TasksRepository,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    private val taskId: String? = savedStateHandle[TodoDestinationsArgs.TASK_ID_ARG]
+
+    private val _uiState = MutableStateFlow(AddEditTaskUiState())
+    val uiState: StateFlow<AddEditTaskUiState> = _uiState.asStateFlow()
+
+    init {
+        if (taskId != null) {
+            loadTask(taskId)
+        }
+    }
+
+    fun saveTask() {
+        if (uiState.value.title.isEmpty() || uiState.value.description.isEmpty()) {
+            _uiState.update {
+                it.copy(userMessage = R.string.empty_task_message)
+            }
+            return
+        }
+
+        if (taskId == null) {
+            createNewTask()
+        } else {
+            updateTask()
+        }
+    }
+
+    fun snackbarMessageShown() {
+        _uiState.update {
+            it.copy(userMessage = null)
+        }
+    }
+
+    fun updateTitle(newTitle: String) {
+        _uiState.update {
+            it.copy(title = newTitle)
+        }
+    }
+
+    fun updateDescription(newDescription: String) {
+        _uiState.update {
+            it.copy(description = newDescription)
+        }
+    }
+
+    private fun createNewTask() = viewModelScope.launch {
+        val newTask = Task(uiState.value.title, uiState.value.description)
+        tasksRepository.saveTask(newTask)
+        _uiState.update {
+            it.copy(isTaskSaved = true)
+        }
+    }
+
+    private fun updateTask() {
+        if (taskId == null) {
+            throw RuntimeException("updateTask() was called but task is new.")
+        }
+        viewModelScope.launch {
+            val updatedTask = Task(
+                title = uiState.value.title,
+                description = uiState.value.description,
+                isCompleted = uiState.value.isTaskCompleted,
+                id = taskId
+            )
+            tasksRepository.saveTask(updatedTask)
+            _uiState.update {
+                it.copy(isTaskSaved = true)
+            }
+        }
+    }
+
+    private fun loadTask(taskId: String) {
+        _uiState.update {
+            it.copy(isLoading = true)
+        }
+        viewModelScope.launch {
+            tasksRepository.getTask(taskId).let { result ->
+                if (result is Result.Success) {
+                    val task = result.data
+                    _uiState.update {
+                        it.copy(
+                            title = task.title,
+                            description = task.description,
+                            isTaskCompleted = task.isCompleted,
+                            isLoading = false
+                        )
+                    }
+                } else {
+                    _uiState.update {
+                        it.copy(isLoading = false)
+                    }
+                }
+            }
+        }
+    }
 }

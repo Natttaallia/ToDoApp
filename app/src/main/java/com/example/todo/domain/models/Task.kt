f9b602afec7374f0765fe4dcf6f87a382a526bcb
nkulbaka <nkulbaka@eclassifiedsgroup.com>
2022-11-03 01:47:17 +0100

    Define Task domain model

--- app/src/main/java/com/example/todo/domain/models/Task.kt
+++ app/src/main/java/com/example/todo/domain/models/Task.kt
@@ -2,4 +2,9 @@ package com.example.todo.domain.models
 
 import java.util.*
 
-data class Task(var id: String = UUID.randomUUID().toString())
+data class Task(
+    var id: String = UUID.randomUUID().toString(),
+    var title: String = "",
+    var description: String = "",
+    var isCompleted: Boolean = false
+)
